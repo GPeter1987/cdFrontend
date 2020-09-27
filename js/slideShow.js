ce3c7a09c360e3de5b5be5aea82c0fd9f294adaf
Gaál Péter <33869458+GPeter1987@users.noreply.github.com>
2020-09-27 20:32:45 +0200

    js & lang setting
    Add slideShow.js
    add lang="hu" to the html pages

---
+++ js/slideShow.js
@@ -0,0 +1,20 @@
+var i  = true;
+slideShow();
+      
+function slideShow() {
+    var c1 = document.getElementById("content1");
+    var c2 = document.getElementById("content2");
+
+    if(i) {
+        c2.style.display ="none";
+        c1.style.display = "block";
+        i = false;
+        setTimeout(slideShow, 9000);
+    }
+    else{
+        c1.style.display ="none";
+        c2.style.display ="block";
+        i = true;
+        setTimeout(slideShow, 9000);
+    }
+}
